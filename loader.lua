c2c8180a14af9646dda082308d787c7489dc6135
XxInsaneX1 <104400273+XxInsaneX1@users.noreply.github.com>
2026-06-19 21:37:17 -0500

    Notify about latest release and fix in GUI
    Added notification for the latest release and changes.

--- loader.lua
+++ loader.lua
@@ -1069,4 +1069,5 @@ global.__AntiFlingGuiController = {
 
 notify("Safety GUI", "Loaded. Anti-Fling is off; Acid-Redirect is ready.")
 notify("WARNING", "This GUI has not been fully Anti-Cheat Tested.", 10)
+notify("Info", "Latest Release 6/19/2026 9:36 PM, Change: Fixed Acid", 60)
 print("[AntiFling] Loaded with synced Acid-Redirect GUI")
